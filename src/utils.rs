8fbcb6e3b81f9502662d810e11912e03f7178997
lilly-lizard <bigbanana29@aol.com>
2023-01-29 08:07:31 +1300

    fixed max target index bug! was bc of shift_vec bounds checking.

--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,7 @@
-
 /// Removes value at `source_idx` and places it at `target_idx`. Does nothing if `source_idx` is
 /// equal to `target_idx` or if either index is outside the vec bounds.
 pub fn shift_vec<T>(source_idx: usize, mut target_idx: usize, vec: &mut Vec<T>) {
-    if source_idx == target_idx || source_idx >= vec.len() || target_idx >= vec.len() {
+    if source_idx == target_idx || source_idx >= vec.len() || target_idx > vec.len() {
         return;
     }
 
